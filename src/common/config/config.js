9190436893c0bb17faf56f7be6e4659ae86b5998
徐青 <qing.xu@QXIT-PC-000600.local>
2020-01-28 16:27:13 +0800

    appid secret从env中获取

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   default_module: 'api',
   weixin: {
-    appid: '', // 小程序 appid
-    secret: '', // 小程序密钥
+    appid: process.env.PINJIAJIA_APPID, // 小程序 appid
+    secret: process.env.PINJIAJIA_SECRET, // 小程序密钥
     mch_id: '', // 商户帐号ID
     partner_key: '', // 微信支付密钥
     notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify
